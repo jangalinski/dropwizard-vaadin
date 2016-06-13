db91e1a137c76964a81f626735a6cd643c1544ab
Volkan Yazici <vyazici@bol.com>
2016-06-13 12:17:54 +0200

    Improve MainUI.

--- src/main/java/de/holisticon/dw/MainUI.java
+++ src/main/java/de/holisticon/dw/MainUI.java
@@ -5,7 +5,7 @@ import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.Title;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.ui.Button;
-import com.vaadin.ui.Label;
+import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 
@@ -16,19 +16,23 @@ public class MainUI extends UI {
     @Override
     protected void init(VaadinRequest vaadinRequest) {
 
-        final VerticalLayout layout = new VerticalLayout();
-        layout.setHeight(100f,Unit.PERCENTAGE);
-        setContent(layout);
+        final TextField counterField = new TextField("Counter", "N/A");
+        counterField.setEnabled(false);
 
-        Button button = new Button("click!", new Button.ClickListener() {
+        Button incrementButton = new Button("Increment", new Button.ClickListener() {
             public void buttonClick(Button.ClickEvent clickEvent) {
                 int count = (Integer) Objects.firstNonNull(getSession().getAttribute("count"), 0);
                 getSession().setAttribute("count", ++count);
-                layout.addComponent(new Label("clicked: " + count ));
+                counterField.setValue(String.format("%d", count));
             }
         });
 
-        layout.addComponent(button);
+        VerticalLayout layout = new VerticalLayout();
+        layout.setSpacing(true);
+        layout.setMargin(true);
+        layout.addComponent(counterField);
+        layout.addComponent(incrementButton);
+        setContent(layout);
 
     }
 
