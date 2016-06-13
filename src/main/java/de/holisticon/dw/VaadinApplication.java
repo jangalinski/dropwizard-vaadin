76afd9127ec0af0225ad87a5c474a00dc499605b
Volkan Yazici <vyazici@bol.com>
2016-06-13 12:00:50 +0200

    Remove unused VaadinApplication.Configuration class.

--- src/main/java/de/holisticon/dw/VaadinApplication.java
+++ src/main/java/de/holisticon/dw/VaadinApplication.java
@@ -3,29 +3,22 @@ package de.holisticon.dw;
 import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.server.VaadinServlet;
 import io.dropwizard.Application;
+import io.dropwizard.Configuration;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 
-public class VaadinApplication extends Application<VaadinApplication.Configuration> {
+public class VaadinApplication extends Application<Configuration> {
 
     @Override
-    public void run(Configuration configuration, Environment environment) throws Exception {
-        // empty
-    }
+    public void run(Configuration configuration, Environment environment) throws Exception {}
 
     @Override
     public void initialize(Bootstrap<Configuration> bootstrap) {
         bootstrap.addBundle(new VaadinBundle(Servlet.class, "/ui/*"));
     }
 
-    public static class Configuration extends io.dropwizard.Configuration {
-        public String name;
-    }
-
     @VaadinServletConfiguration(ui = MainUI.class, productionMode = false)
-    public static class Servlet extends VaadinServlet {
-        // empty
-    }
+    public static class Servlet extends VaadinServlet {}
 
     public static void main(String... args) throws Exception {
         new VaadinApplication().run(args);
